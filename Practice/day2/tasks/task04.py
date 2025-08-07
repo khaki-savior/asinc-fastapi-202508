56fcd5b8e86d4c9fda6037c5329da4c898b45a56
khaki-savior <vika.esya@gmail.com>
2025-08-07 14:04:44 +0300

    Task04 solution added

--- Practice/day2/tasks/task04.py
+++ Practice/day2/tasks/task04.py
@@ -3,3 +3,23 @@
 #  Каждая функция должна возвращать случайное целое число в диапазоне от 0 до 10
 #  и выполняться за случайное время от 1 до 5 секунд
 #  После завершения всех функций найдите сумму всех полученных результатов и выведите её на экран.
+
+import asyncio
+import random
+
+async def wait_random(task_id: int) -> int:
+    base_delay = random.randint(1, 5)
+    await asyncio.sleep(base_delay)
+    random_number = random.randint(0, 10)
+    print(f"Результат задачи {task_id}: {random_number} (задержка: {base_delay:.1f}с)")
+    return random_number
+
+async def main():
+    num_tasks = 4
+    tasks = [asyncio.create_task(wait_random(i)) for i in range(num_tasks)]
+    
+    total_sum = await asyncio.gather(*tasks)
+    print(f'Получен результат: {sum(total_sum)}')
+
+if __name__ == '__main__':
+    asyncio.run(main())

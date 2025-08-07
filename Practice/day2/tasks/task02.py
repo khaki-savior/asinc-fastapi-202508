7c2736582ce340d53cb90ff4c2fa037b11de9868
khaki-savior <vika.esya@gmail.com>
2025-08-07 12:22:16 +0300

    Task02 solution added

--- Practice/day2/tasks/task02.py
+++ Practice/day2/tasks/task02.py
@@ -1,2 +1,22 @@
 # TODO: Создайте 4 корутины, каждая из которых возвращает строку.
 #  Запустите их параллельно и соберите результаты в список.
+
+import asyncio
+import random
+
+async def get_text():
+    multiplier = random.randint(1,5)
+    await asyncio.sleep(multiplier)
+    #print(multiplier)
+    return multiplier*'42'
+
+async def main():
+    num_tasks = 4
+
+    tasks = [asyncio.create_task(get_text()) for _ in range(num_tasks)]
+    results = await asyncio.gather(*tasks)
+
+    print("Результаты:", results)
+
+if __name__ == '__main__':
+    asyncio.run(main())

7417c135b387a991e6c37379387be5a678246295
khaki-savior <vika.esya@gmail.com>
2025-08-07 15:04:58 +0300

    Task05 solution added

--- Practice/day2/tasks/task05.py
+++ Practice/day2/tasks/task05.py
@@ -1,2 +1,27 @@
 # TODO: Создайте корутину, которая ждет 5 секунд.
 #  Запустите ее как задачу и отмените ее через 2 секунды.
+
+import asyncio
+
+
+async def my_coroutine(n: int):
+    await asyncio.sleep(5)
+    print(f'Корутина {n} завершилась.')
+    return f"Результат {n}"
+
+async def main():
+    task = asyncio.create_task(my_coroutine(1))
+
+    try:
+        result = await asyncio.wait_for(task, timeout=2.0)
+    except asyncio.TimeoutError:
+        task.cancel()
+        try:
+            await task  # Ждем !!завершения ОТМЕНЫ!!
+        except asyncio.CancelledError:
+            print("Задача была отменена")
+
+    print(f"Статус задачи: отменена={task.cancelled()}, завершена={task.done()}")
+
+if __name__ == '__main__':
+    asyncio.run(main())

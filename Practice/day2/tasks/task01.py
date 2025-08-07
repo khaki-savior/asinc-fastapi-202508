13c8a2edd86d3d8173bb0ca055b6a86c916afa19
khaki-savior <vika.esya@gmail.com>
2025-08-07 09:51:23 +0300

    Task01 solution added

--- Practice/day2/tasks/task01.py
+++ Practice/day2/tasks/task01.py
@@ -5,7 +5,20 @@ async def wait_random():
     delay = random.randint(1, 5)
     await asyncio.sleep(delay)
     print(f"Задача завершилась через {delay} секунд")
+    return delay
 
 
 # TODO: дана асинхронная функция которая ждет случайное количество секунд (от 1 до 5)
 #  Создайте 3 корутины, и запустите их асинхронно с помощью asyncio.gather().
+
+async def main():
+    # Создаем 3 задачи
+    task1 = asyncio.create_task(wait_random())
+    task2 = asyncio.create_task(wait_random())
+    task3 = asyncio.create_task(wait_random())
+    
+    results = await asyncio.gather(task1, task2, task3)
+    #print("Результаты:", *results)
+
+if __name__ == '__main__':
+    asyncio.run(main())

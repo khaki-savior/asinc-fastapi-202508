0c54af27b3bb5b761ab05badce2cc1501f9261a2
khaki-savior <vika.esya@gmail.com>
2025-08-13 21:09:13 +0300

    Practice/day3/main3_2: added GET/books and GET/books/{book_id}

--- Practice/day3/main3_2.py
+++ Practice/day3/main3_2.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
+from typing import Optional
+import asyncio
+
+class Book(BaseModel):
+    title: str
+    author: Optional[str] = None
+
+books_db = {
+    1:{"title": "Война и мир", "author": "Толстой Л.Н."}
+}    
 
 app = FastAPI(
     title="FastAPI 'Hello World!'",
@@ -20,25 +31,30 @@ def root():
     """
     return hello_msg
 
-@app.get_books("GET/books")
-def get_books():
-    pass
+@app.get("/books")
+async def get_books():
+    books_to_show = books_db
+    return books_to_show
 
-@app.get_book_by_id("GET/books/{book_id}")
-def get_book_by_id():
-    pass
+@app.get("/books/{book_id}")
+async def get_book_by_id(book_id: int):
+    book_data = books_db[book_id]
+    book = Book(**book_data)
+    return book
 
-@app.postbooks("POST/books")
+@app.post("/books")
 def add_books():
     pass
 
-@app.put_books("PUT/books/{book_id}")
+@app.put("/books/{book_id}")
 def update_book_by_id():
     pass
 
-@app.del_books("DELETE /books/{book_id}")
+@app.delete("/books/{book_id}")
 def update_book_by_id():
     pass
 
 if __name__ == '__main__':
-    uvicorn.run("main3_1:app", host="0.0.0.0", port=8000, log_level="debug", reload=True)
+    #uvicorn.run("main3_2:app", host="0.0.0.0", port=8000, log_level="debug", reload=True)
+    #print(asyncio.run(get_books()))
+    #print(asyncio.run(get_book_by_id(1)))

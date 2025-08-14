4aabdf709718fc80e93d33e51a48c89520e9eea6
khaki-savior <vika.esya@gmail.com>
2025-08-14 09:11:11 +0300

    Practice/day3/main3_2: added DELETE/books/{book_id}

--- Practice/day3/main3_2.py
+++ Practice/day3/main3_2.py
@@ -64,10 +64,15 @@ def update_book_by_id(book_id: int, book: Book, response: Response):
     return {"detail": "Book updated successfully"}
 
 @app.delete("/books/{book_id}")
-def update_book_by_id():
-    pass
+def delete_book_by_id(book_id: int, response: Response):
+    if book_id not in books_db:
+        response.status_code = 404
+        return {"detail": "Book not found"}
+    
+    deleted_book = books_db.pop(book_id) 
+    return {"message": "Book deleted successfully", "deleted_book": deleted_book}
 
 if __name__ == '__main__':
-    #uvicorn.run("main3_2:app", host="0.0.0.0", port=8000, log_level="debug", reload=True)
+    uvicorn.run("main3_2:app", host="0.0.0.0", port=8000, log_level="debug", reload=True)
     #print(asyncio.run(get_books()))
     #print(asyncio.run(get_book_by_id(1)))
